dc85fb0cac462391cc60c32cca7192f6e86151e3
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-10-29 15:35:06 +0530

    created server

---
+++ ClientHandler.java
@@ -0,0 +1,12 @@
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable{
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+
+    @Override
+    public void run(){
+
+    }
+}
