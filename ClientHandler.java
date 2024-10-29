e0e8b35455e1a6c4738f91ca54e487c819114436
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-10-29 15:51:10 +0530

    implemented client handler

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,12 +1,48 @@
-import java.sql.ClientInfoStatus;
+import java.io.*;
+import java.net.Socket;
 import java.util.ArrayList;
 
-public class ClientHandler implements Runnable{
+public class ClientHandler implements Runnable {
 
     public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUsername;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUsername = bufferedReader.readLine();
+            clientHandlers.add(this);
+            broadcastMessage("SERVER: " + clientUsername + " has entered the chat")
+        } catch (IOException e) {
+            closeEverything(socket, bufferedWriter, bufferedReader);
+        }
+    }
 
     @Override
-    public void run(){
+    public void run() {
+        String messageFromClient;
+
+        while (socket.isConnected()) {
+            try {
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket, bufferedWriter, bufferedReader);
+                break;
+            }
+        }
+    }
+
+    public void broadcastMessage(String messageToSend) throws IOException {
+        for (ClientHandler clientHandler : clientHandlers) {
+            this.bufferedWriter.write(messageToSend);
+
+        }
 
     }
 }
